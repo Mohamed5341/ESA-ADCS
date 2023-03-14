46686991800c1c751ac28a7548a9428afbcac8ab
Mohamed Ashraf <ashrafmohamed534@gmail.com>
2023-03-14 05:47:22 +0200

    add blink led example

---
+++ Source/Blink_LED/Blink_LED.ino
@@ -0,0 +1,18 @@
+/*
+ * Blink LED Example
+ */
+#define LED 13
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(3000);
+
+  digitalWrite(LED, LOW);
+  delay(3000);
+}
